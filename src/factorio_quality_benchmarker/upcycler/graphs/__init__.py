b0d4204477c6a7de6bdc4b51725449afa4f2c8ab
Max Hawkins <m@ximilian.uk>
2026-09-09 17:16:50 +0100

    File name change

--- src/factorio_quality_benchmarker/upcycler/graphs/__init__.py
+++ src/factorio_quality_benchmarker/upcycler/graphs/__init__.py
@@ -1,2 +1,2 @@
 from .models import ProductionGraph, RecipeGraphIndex, UpcyclingGraph  # noqa: F401
-from .recipe_graphs import generate_recipe_graph_index  # noqa: F401
+from .search import generate_recipe_graph_index  # noqa: F401
